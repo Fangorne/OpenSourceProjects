48afc59cb718df588238ae847be2c5dec6ccda5c
Fangorne <marcus_the@msn.com>
2022-11-20 14:14:57 +0100

    Add project files.

---
+++ FluentDockerExample/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Ductus.FluentDocker.Builders;
+
+Console.WriteLine("Hello, World!");
+
+var svc = new Builder()
+    .UseContainer()
+    .UseCompose()
+    .FromFile("./docker-compose.yml")
+    .RemoveOrphans()
+    .Build().Start();
+
+Task.Delay(2000);
+
+svc.Stop();
+svc.Remove(true);
---
+++ Tests/FluentDockerExample.Tests/TestWithFluentDocker.cs
@@ -0,0 +1,56 @@
+﻿using Ductus.FluentDocker.Builders;
+using Ductus.FluentDocker.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StackExchange.Redis;
+using Xunit;
+
+namespace FluentDockerExample.Tests;
+
+public class DockerFixture : IDisposable
+{
+    private ICompositeService? CompositeService { get; }
+
+    public DockerFixture()
+    {
+        CompositeService = new Builder()
+            .UseContainer()
+            .UseCompose()
+            .FromFile("./docker-compose.yml")
+            .RemoveOrphans()
+            .Build()
+            .Start();
+    }
+
+    public void Dispose()
+    {
+        CompositeService?.Stop();
+        CompositeService?.Remove(true);
+    }
+}
+
+public class TestWithFluentDocker : IClassFixture<DockerFixture>
+{
+    #region fields
+
+    private static readonly ConnectionMultiplexer Redis = ConnectionMultiplexer.Connect(
+        new ConfigurationOptions
+        {
+            EndPoints = {"localhost:7000"}
+        });
+
+    #endregion
+
+    #region
+
+   
+    [Fact]
+    public void TestRedis()
+    {
+        var db = Redis.GetDatabase();
+        var pong = db.Ping();
+
+        Assert.IsNotNull(db);
+    }
+
+    #endregion
+}
